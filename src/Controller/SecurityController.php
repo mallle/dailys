5089abcaf60c654a457fc4a455e08179c7942631
Malene <maleneklit@hotmail.com>
2020-02-17 20:14:06 +0100

    Add reset password function and a command to add new users

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Security\LoginFormAuthenticator;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends BaseController
@@ -26,6 +30,7 @@ class SecurityController extends BaseController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
+            'navi' => 'login',
             'last_username' => $lastUsername,
             'error' => $error
         ]);
@@ -39,5 +44,52 @@ class SecurityController extends BaseController
     public function logout()
     {
         throw new \Exception('Will be intercepted before getting here');
+    }
+
+    /**
+     * @Route("/reset-password", name="app_reset_password")
+     *
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param GuardAuthenticatorHandler $guardHandler
+     * @param LoginFormAuthenticator $formAuthenticator
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response|null
+     */
+    public function resetPassword(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator, AuthenticationUtils $authenticationUtils)
+    {
+        if($this->getUser() === null){
+            return $this->redirect($this->generateUrl('app_login'));
+        }
+
+        $error = '';
+        if ($request->isMethod('POST')) {
+            $user = $this->getUser();
+
+            if($passwordEncoder->isPasswordValid($user, $request->request->get('oldPassword'))){
+
+                $user->setPassword($passwordEncoder->encodePassword(
+                    $user,
+                    $request->request->get('newPassword')
+                ));
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+
+                return $guardHandler->authenticateUserAndHandleSuccess(
+                    $user,
+                    $request,
+                    $formAuthenticator,
+                    'main'
+                );
+            }
+            $error = 'Your old password was not correct';
+        }
+
+        return $this->render('security/reset_password.html.twig', [
+            'navi' => 'change-password',
+            'error' => $error,
+        ]);
     }
 }
