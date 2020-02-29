7a0af55aad01036a5d04616e16d46506b095c085
Malene <maleneklit@hotmail.com>
2020-02-29 15:07:05 +0100

    Update TransferDataCommand.php

--- src/Command/TransferDataCommand.php
+++ src/Command/TransferDataCommand.php
@@ -51,7 +51,7 @@ class TransferDataCommand extends Command
         $user2 = $this->userRepository->find('12');
 
         foreach ($user1->getHabits() as $habit){
-            if($habit->getId() === '32'){
+            if($habit->getId() === 32){
                 for($i = 1; $i <= 29; $i++ ){
                     $date = new \DateTime( $i . ' February 2020');
 
@@ -60,12 +60,12 @@ class TransferDataCommand extends Command
                     $checked->setCheckedAt($date);
                     $this->entityManager->persist($checked);
                 }
+                $this->entityManager->flush();
             }
-            $this->entityManager->flush();
         }
 
         foreach ($user2->getHabits() as $habit){
-            if($habit->getId() === '62'){
+            if($habit->getId() === 62){
                 for($i = 1; $i <= 29; $i++ ){
                     $date = new \DateTime( $i . ' February 2020');
                     $checked = new Checked();
@@ -75,7 +75,7 @@ class TransferDataCommand extends Command
                 }
                 $this->entityManager->flush();
             }
-            if($habit->getId() === '52'){
+            if($habit->getId() === 52){
                 for($i = 1; $i <= 29; $i++ ){
                     $date = new \DateTime( $i . ' February 2020');
                     $checked = new Checked();
